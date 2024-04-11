3b90309bc10602de246bed1a35f5866818ba7844
Ad450 <adjeimanue@gmail.com>
2024-04-11 16:26:11 +0000

    remove unneeded comments

--- core/lib/domain/repository/practitioner.dart
+++ core/lib/domain/repository/practitioner.dart
@@ -5,9 +5,3 @@ abstract class PractitionerRepository {
   //TODO: change [day] to the Day object that will be used with the practitioner's Calendar
   Future<VoidType> changeAvailability(Day day);
 }
-
-//
-// firebase_core: ^2.9.0
-// firebase_auth: ^4.4.0
-// firebase_storage: ^11.1.0
-// cloud_firestore: ^4.5.0
